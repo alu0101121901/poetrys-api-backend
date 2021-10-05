6248829d3c32f823bef2254c0fb24915fa9c8227
Rafa <alu0101121901@ull.edu.es>
2021-10-05 12:26:00 +0100

    Added some close MongoDB connection lines

--- index.js
+++ index.js
@@ -1,136 +1,116 @@
-require('dotenv').config()
-require('./mongo')
+require("dotenv").config();
+// require('./mongo')
 
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const logger = require('./loggerMiddleware')
+const mongoose = require("mongoose");
 
-const Poetry = require('./model/Poetry')
+const connectionString = process.env.MONGO_DB_URI;
+
+mongoose
+  .connect(connectionString)
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const logger = require("./loggerMiddleware");
+
+const Poetry = require("./model/Poetry");
 
 // Usamos cors
-app.use(cors())
-app.use(express.json()) 
-app.use(logger)
+app.use(cors());
+app.use(express.json());
+app.use(logger);
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello world</h1>');
-})
+app.get("/", (request, response) => {
+  response.send("<h1>Hello world</h1>");
+});
 
-app.get('/api/poetryArray', (request, response) => {
-  Poetry.find({})
-    .then(poetrys => {
-      response.json(poetrys)
-    })
-})
+app.get("/api/poetryArray", (request, response) => {
+  Poetry.find({}).then((poetrys) => {
+    console.log(poetrys);
+    response.json(poetrys);
+    mongoose.connection.close();
+  });
+});
 
-app.post('/api/poetryArray', (request, response) => {
-  const poetry = request.body
+app.post("/api/poetryArray", (request, response) => {
+  const poetry = request.body;
 
   if (!poetry.content || !poetry.title) {
     return response.status(400).json({
-      error: 'required "content" or/and "title" are missing'
-    })
+      error: 'required "content" or/and "title" are missing',
+    });
   }
 
   const newPoetry = new Poetry({
     title: poetry.title,
     content: poetry.content,
     likes: 0,
-  })
+  });
 
-  newPoetry.save()
-    .then(savedNote => {
-      response.json(savedNote)
-    })
-})
-
-app.get('/api/poetryArray/:id', (request, response, next) => {
-  const { id } = request.params
-
-  Poetry.findById(id).then(poetry => {
-    if (poetry) {
-      return response.json(poetry)
-    }
-    else {
-      response.status(404).end()
-    }
-  }).catch(err => {
-    next(err)
-  })
+  newPoetry.save().then((savedNote) => {
+    response.json(savedNote);
+    mongoose.connection.close();
+  });
 });
 
-app.delete('/api/poetryArray/:id', (request, response, next) => {
-  const id = request.params
+app.get("/api/poetryArray/:id", (request, response, next) => {
+  const { id } = request.params;
+
+  Poetry.findById(id)
+    .then((poetry) => {
+      if (poetry) {
+        return response.json(poetry);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+app.delete("/api/poetryArray/:id", (request, response, next) => {
+  const id = request.params;
 
   Poetry.findByIdAndRemove(id)
-    .then(result => {
-      console.log(result)
+    .then((result) => {
+      console.log(result);
       response.status(204).end();
-    }).catch(err => {
-      next(err)
+      mongoose.connection.close();
     })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 // eslint-disable-next-line no-unused-vars
 app.use((error, request, response, next) => {
-  console.error(error)
-  console.error(error.name)
-  if(error.name === 'CastError') {
+  console.error(error);
+  console.error(error.name);
+  if (error.name === "CastError") {
     response.status(400).send({
-      error: 'id used is malformed'
-    })
+      error: "id used is malformed",
+    });
   } else {
-    response.status(500).end()
+    response.status(500).end();
   }
-})
+});
 
 app.use((request, response) => {
-  console.log(request.path)
+  console.log(request.path);
   response.status(404).json({
-    error: 'Not found'
+    error: "Not found",
   });
-})
+});
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-/* let poetryArray = [
-  {
-    id: 1,
-    title: "Revolucionaria",
-    content: ["Latidos que borran fronteras.", 
-      "Mirada que refleja libertad.",
-      "Su piel como color de mi bandera", 
-      "y su voz como mi himno nacional.",
-      "/br",
-      "Pasos que dejan grandes huellas,",
-      "manos que albergan en un amplio hogar",
-      "y en su espalda residen las estrellas",
-      "que mis yemas ansían tanto conquistar.",
-      "/br",
-      "Su regazo regala una enorme trinchera",
-      "que me reguarda de todo mal",
-      "y en sus abrazos encuentro la tregua",
-      "frente a cualquier pensamiento radical.",
-      "/br",
-      "En sus ojos, eterno esbozo de sinceridad.",
-      "Contra ella perdería toda guerra,",
-      "pues a su vera obtengo la mayor paz",
-      "que se pueda sentir sobre La Tierra."],
-    likes: 51,
-  },
-  {
-    id: 2,
-    title: "Dudas",
-    content: ["Ya no sé si se apaga o se enciende esta lumbre.", 
-      "No sé si tus descargas me dan luz o calambres.",
-      "Mi única certeza es que me amordaza la incertidumbre", 
-      "y las bocas cerradas se mueren de hambre.",
-    ],
-    likes: 12,
-  },
-] */
--- mongo.js
+++ mongo.js
@@ -10,31 +10,7 @@ mongoose.connect(connectionString)
   })
 
 // Se debe definir un esquema para el poema
-
-/* const poetry = new Poetry({
-  title: "Revolucionaria",
-  content: ["Latidos que borran fronteras.", 
-    "Mirada que refleja libertad.",
-    "Su piel como color de mi bandera", 
-    "y su voz como mi himno nacional.",
-    "/br",
-    "Pasos que dejan grandes huellas,",
-    "manos que albergan en un amplio hogar",
-    "y en su espalda residen las estrellas",
-    "que mis yemas ansían tanto conquistar.",
-    "/br",
-    "Su regazo regala una enorme trinchera",
-    "que me reguarda de todo mal",
-    "y en sus abrazos encuentro la tregua",
-    "frente a cualquier pensamiento radical.",
-    "/br",
-    "En sus ojos, eterno esbozo de sinceridad.",
-    "Contra ella perdería toda guerra,",
-    "pues a su vera obtengo la mayor paz",
-    "que se pueda sentir sobre La Tierra."],
-  likes: 51,
-})
-
+/*
 poetry.save()
   .then(result => {
     console.log(result)
